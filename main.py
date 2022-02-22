25272405ab06e9fe9c5544d496ddffc4c027806f
StuartLittleKL <100061895+StuartLittleKL@users.noreply.github.com>
2022-02-22 21:59:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from pyrogram import Client, filters
 
+
+
+Stuart=Client(
+    "Pyrogram Bot",
+    bot_token="bot token",
+    api_id="api id",
+    api_hash="api hash"
+)
+
+@Stuart.on_message(filters.command("start"))
+async def start_message(bot, message):
+    await message.reply_text("hi")
+
+Stuart.run()
