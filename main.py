5af01cf8d6d0607c679af7e45c64c36e128fc6ea
StuartLittleKL <100061895+StuartLittleKL@users.noreply.github.com>
2022-02-22 23:10:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from pyrogram import Client, filters
 
 Stuart=Client(
     "Pyrogram Bot",
-    bot_token="bot token",
-    api_id="api id",
-    api_hash="api hash"
+    bot_token="5153898163:AAE-DNHYOwy7xze2NZbDiSveDh8cUEgbnL4",
+    api_id="9297449",
+    api_hash="f8c528c480bcccd28df74a645275e446"
 )
 
 @Stuart.on_message(filters.command("start"))
