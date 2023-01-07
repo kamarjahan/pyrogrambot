3b4834d201e8ae5cb028c4be66172fccbf0d20f2
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2023-01-07 23:02:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,50 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
+import os
+import requests
+from PIL import Image
+from io import BytesIO
 
+from telegram.ext import Updater, MessageHandler, Filters
 
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm an autofilter bot. Send me a picture and I'll apply a filter to it.")
 
-Stuart=Client(
-    "Pyrogram Bot",
-    bot_token="5153898163:AAE-DNHYOwy7xze2NZbDiSveDh8cUEgbnL4",
-    api_id="9297449",
-    api_hash="f8c528c480bcccd28df74a645275e446"
-)
+def filter_picture(update, context):
+    # Check if the message is a photo
+    if update.message.photo:
+        # Get the largest photo from the list of photos
+        photo = update.message.photo[-1]
+        file_id = photo.file_id
+        file = context.bot.get_file(file_id)
+        file_url = file.file_path
+        # Download the photo
+        response = requests.get(f"https://api.telegram.org/file/bot{TOKEN}/{file_url}")
+        image = Image.open(BytesIO(response.content))
+        # Apply a filter to the photo
+        image = image.filter(ImageFilter.BLUR)
+        # Save the filtered photo to a file
+        image.save("filtered_photo.jpg")
+        # Send the filtered photo back to the user
+        context.bot.send_photo(chat_id=update.effective_chat.id, photo=open("filtered_photo.jpg", "rb"))
 
-@Stuart.on_message(filters.command("start"))
-async def start_message(bot, message):
-    await message.reply_text("Hey🥰 bro sugamano")
+def main():
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    updater = Updater(5849769037:AAFsz6rZz11ia8RxE9Lc1rEHm3EBYlV4OME, use_context=True)
 
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
 
-@Stuart.on_message(filters.command("help"))
-async def help(bot: Stuart, message: Message):
-    await message.reply_text("Help🗿")
+    # Add conversation handler with the states CHOOSING, TYPING_CHOICE and TYPING_REPLY
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.photo, filter_picture))
 
-Stuart.run()
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
